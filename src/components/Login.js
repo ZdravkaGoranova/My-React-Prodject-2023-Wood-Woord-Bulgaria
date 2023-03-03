619a53f0f0f50ab0e176bf62a2825de082b1c1df
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-04 00:11:22 +0200

    Update login and register

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,28 @@
 import '../css/register-login.css'
 import '../css/site.css'
 
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+
+
 export default function Login() {
+
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e => {
+        e.preventDefault();
+
+        // auth
+        //     .signInWithEmailAndPassword(email, password)
+        //     .then(auth => {
+        //         navigate.push('/');
+        //     })
+        //     .catch(error => alert(error.message))
+
+    }
+
     return (
         <section id="login-container">
             <div class="container">
@@ -13,12 +34,12 @@ export default function Login() {
                     <p>Welcome, see the new masterpieces!</p>
 
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" placeholder="ivan_00" name="username" />
+                    <input type="text" id="username" placeholder="ivan_00" name="username" value={username} />
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" placeholder="*****" name="password" />
+                    <input type="password" id="password" placeholder="*****" name="password" value={password} />
 
-                    <button class="login-btn">Login</button>
+                    <button class="login-btn" type='submit' onClick={signIn} >Login</button>
                     <div class="card-no-account">
                         <p>Don't have an account? <a href="/register">Sign up</a>.</p>
                     </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,7 +2,31 @@ import '../css/register-login.css'
 import '../css/site.css'
 
 
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+
 export default function Register() {
+    const navigate = useNavigate ();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
+
+    const register = e => {
+        e.preventDefault();
+
+        // auth
+        //     .createUserWithEmailAndPassword(email, password)
+        //     .then((auth) => {
+        //         if (auth) {
+        //             navigate.push('/');
+        //         }
+        //     })
+        //     .catch(error => alert(error.message))
+
+    }
+
+
     return (
         <section id="register-container">
             <div className="register-container-info">
@@ -14,10 +38,10 @@ export default function Register() {
                     <p>Register to get ideas and view the latest masterpieces.</p>
 
                     <label htmlFor="username">Username:</label>
-                    <input type="text" id="username" placeholder="ivan_00" name="username" />
+                    <input type="text" id="username" placeholder="ivan_00" name="username" value={username} />
 
                     <label htmlFor="password">Password:</label>
-                    <input type="password" id="password" placeholder="*****" name="password" />
+                    <input type="password" id="password" placeholder="*****" name="password"  value={password}/>
 
                     <label htmlFor="re-password">Repeat password:</label>
                     <input type="password" id="re-password" placeholder="*****" name="confirmPassword" />
@@ -25,7 +49,7 @@ export default function Register() {
                     {/* <label htmlFor="address">Address:</label>
                     <input type="text" id="address" placeholder="2572 Orphan Road" name="address" /> */}
 
-                    <button className="register-btn">Register</button>
+                    <button className="register-btn" type='submit' onClick={register}>Register</button>
                     <div className="card-no-account">
                         <p>Already have an account?<a href="/login" /> Sign in</p>
                     </div>
