4a2ebf7fd425a70873e801b31bda0c2095e808ad
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-10 21:06:09 +0200

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,14 +2,16 @@
 
 //import firebase from "firebase";
 
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import * as firebase from "firebase/app";
+
+// import { initializeApp } from "firebase/app";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 
 // import { getFirestore } from "@firebase/firestore";
 
-const firebaseApp = initializeApp({
+const app = firebase.initializeApp({
     apiKey: "AIzaSyBeS-DTjHTdkTThggh1ZGBwsI5tj-Rl4tw",
     authDomain: "woodworkingproduct.firebaseapp.com",
     projectId: "woodworkingproduct",
@@ -18,28 +20,29 @@ const firebaseApp = initializeApp({
     appId: "1:588405437242:web:f996336decda3289f85603"
 });
 
+export default app;
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 // export const firestore = getFirestore(app);
 
 
-const auth = getAuth(firebaseApp);
+// const auth = getAuth(firebaseApp);
 
-// const db = getFirestore(firebaseApp);
-// db.collection('products').getDocs();
-// const productCol = collection(db, 'products');
-// const snapshot = await getDocs(productCol);
+// // const db = getFirestore(firebaseApp);
+// // db.collection('products').getDocs();
+// // const productCol = collection(db, 'products');
+// // const snapshot = await getDocs(productCol);
 
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
-// export { db, auth }
+// // const firebaseApp = firebase.initializeApp(firebaseConfig);
+// // const db = firebaseApp.firestore();
+// // const auth = firebase.auth();
+// // export { db, auth }
 
 
-onAuthStateChanged(auth, user => {
-    if (user !== null) {
-        console.log('Logged in!')
-    } else {
-        console.log('No user')
-    }
-})
+// onAuthStateChanged(auth, user => {
+//     if (user !== null) {
+//         console.log('Logged in!')
+//     } else {
+//         console.log('No user')
+//     }
+// })
