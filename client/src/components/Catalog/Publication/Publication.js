756d9a7811fd7ad55c3a165d03c9ab0835e50764
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-15 01:14:01 +0200

    add new folder

---
+++ client/src/components/Catalog/Publication/Publication.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../gallery.css'
+
+import { Link } from 'react-router-dom'
+
+export default function Publication({
+    _id,
+    title,
+    description,
+    picture,
+    price,
+    isCompleted,
+}) {
+    return (
+        <article className="card-info">
+            <article className="card-info-image">
+                <img src={picture} alt={title} />
+            </article>
+            <article className="card-info-text">
+                <h2>Title: {title}</h2>
+                <Link to={`/details/${_id}`}  className="details-btn">Details</Link>
+            </article>
+        </article>
+    )
+}
