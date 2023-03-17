6d5a3c251e3972c201ed3a9003d115a2514481dc
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-17 16:15:11 +0200

    update

--- client/src/components/Edit/Edit.js
+++ client/src/components/Edit/Edit.js
@@ -1,20 +1,16 @@
 import '../Edit/create-edit.css'
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import * as productService from '../../services/productService.js'
 import { useParams } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 
+import { WoodContext } from '../../contexts/WoodContext.js'
 const baseUrl = 'http://localhost:3030/jsonstore/woodTypes';
 
-export default function Edit({
-    // id,
-    // title,
-    // description,
-    // picture,
-    // price,
-    // type
-}) {
+export default function Edit() {
+    const { updateProduct } = useContext(WoodContext)
 
     const { productId } = useParams();
     console.log(productId);
@@ -29,63 +25,41 @@ export default function Edit({
 
     });
 
-
     useEffect(() => {
-        fetch(`${baseUrl}/${productId}`)
-            .then(res => res.json())
+        productService.update(productId)
             .then(data => {
                 // const product = data['productId'];
                 console.log(data)
                 console.log(':)')
-
                 setProduct(data)
             })
     }, [productId]);
 
 
-    const update = async (productId, product) => {
-        const { ...data } = product;
-
-        const response = await fetch(`${baseUrl}/${productId}`, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        });
-
-        const result = await response.json();
-
-        return result.user;
-    };
-
-    const onSubmitHandler = (e, productData) => {
+    const onSubmitHandler = async (e, productData) => {
         e.preventDefault();
 
         console.log(':):):)')
-        if (!productData.picture.startsWith("https://")) {
-            alert("Please enter a valid URL address");
-        } else {
-            try {
-                update(productId, productData)
-                    .then(() => {
-                        navigate(`/details/${productId}`, { replace: true });
-                    });
-            } catch (err) {
-                alert(err);
-            }
-        }
+        await updateProduct(productId, productData)
+        //или
+        // if (!productData.picture.startsWith("https://")) {
+        //     alert("Please enter a valid URL address");
+        // } else {
+        //     try {
+        //        await  productService.update(productId, productData)
+        //             .then(() => {
+        //                 navigate(`/details/${productId}`, { replace: true });
+        //             });
+        //     } catch (err) {
+        //         alert(err);
+        //     }
+        // }
 
     };
     const onChangeHandler = (e) => {
         setProduct(state => ({ ...state, [e.target.name]: e.target.value }))
     };
 
-    const onEdit = e => {
-        e.preventDefault();
-
-
-    };
     return (
 
         <section id="edit-container">
@@ -99,48 +73,18 @@ export default function Edit({
                     <p>Edit your wood product!</p>
 
                     <label htmlFor="title">Title:</label>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        value={product?.title}// value={product.title}
-                        onChange={onChangeHandler}
-                    />
+                    <input type="text" id="title" name="title" value={product?.title} onChange={onChangeHandler} />
 
                     <label htmlFor="painting-tech">Description:</label>
-                    <input
-                        type="text"
-                        id="painting-tech"
-                        name="description"
-                        value={product?.description}
-                        onChange={onChangeHandler}
-                    />
+                    <input type="text" id="painting-tech" name="description" value={product?.description} onChange={onChangeHandler} />
 
                     <label htmlFor="picture">Wood picture:</label>
-                    <input
-                        type="text"
-                        id="picture"
-                        name="picture"
-                        value={product?.picture}// value={product.picture}
-                        onChange={onChangeHandler}
-                    />
+                    <input type="text" id="picture" name="picture" value={product?.picture} onChange={onChangeHandler} />
 
                     <label htmlFor="certificate">Price:</label>
-                    <input type="text"
-                        id="certificate"
-                        placeholder="Yes"
-                        name="price"
-                        value={product?.price}
-                        onChange={onChangeHandler}
-                    />
+                    <input type="text" id="certificate" placeholder="Yes" name="price" value={product?.price} onChange={onChangeHandler} />
                     <label htmlFor="type">Type:</label>
-                    <select
-                        type="type"
-                        id="certificate"
-                        name="type"
-                        value={product?.type}
-                        onChange={onChangeHandler}
-                    >
+                    <select type="type" id="certificate" name="type" value={product?.type} onChange={onChangeHandler} >
                         <option value="spoons" name="spoons">Spoons</option>
                         <option value="chairs" name="chairs">Chairs</option>
                         <option value="ladles" name="ladles">Ladles</option>
