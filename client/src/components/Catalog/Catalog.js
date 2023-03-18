afa6e6423af445629bd13d8dedb6ad50c2d4d12d
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-18 19:49:21 +0200

    update

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,49 +1,24 @@
 import '../Catalog/gallery.css'
 
-import Publication from './Publication.js'
-import React, { useEffect, useState } from "react"
+import Publication from './Publication/Publication.js'
+import React, { useEffect, useState, useContext } from "react"
 import { Link } from 'react-router-dom';
-
-const baseUrl = 'http://localhost:3030/jsonstore/woodTypes';
-
-export default function Catalog({
-    //data,
-}) {
-    console.log(':)')
-    const [products, setProducts] = useState([])
-
-    useEffect(() => {
-        fetch(`${baseUrl}`)
-            .then(res => res.json())
-            .then(data => {
-                // console.log(data)
-                // console.log(data.products)
-                console.log(Object.values(data.products))
-                setProducts(Object.values(data.products))
-
-            })
-    }, []);
-
-    console.log(Object.values(products))
-    console.log(typeof products)
+import { WoodContext } from '../../contexts/WoodContext.js'
+export default function Catalog() {
+  
+    const { products } = useContext(WoodContext)
+    console.log(products)
+    //console.log(typeof products)
 
     // const filteredItems = products.filter(item => item.type === 'toolboxes');
     // console.log(filteredItems);
 
-    // const sortedObjects = products.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-
-    // // използваме slice(), за да вземем първите 3 елемента от сортирания масив и reverse(), за да ги обърнем в правилния ред
-    // const lastThreeObjects = sortedObjects.slice(0, 3).reverse();
-    // const lastThreeObjects = products.slice(-3)
-    // console.log(lastThreeObjects);
-
-    //console.log(typeof products)
     return (
 
         < section id="gallery" >
             <h1>Wood World Gallery</h1>
             <article className="gallery-container">
-                <Link to={`/catalog/Spoons`} className="btn-catalog" type='submit' >Spoons</Link>
+                <Link to={`/catalog/Spoons`} className="btn-catalog" type='submit' products={products} >Spoons</Link>
                 <Link to={`/catalog/Chairs`} className="btn-catalog" type='submit' >Chairs</Link>
                 <Link to={`/catalog/Ladles`} className="btn-catalog" type='submit' >Ladles</Link>
                 <Link to={`/catalog/Furnitures`} className="btn-catalog" type='submit' >Furnitures </Link>
@@ -51,25 +26,19 @@ export default function Catalog({
                 <Link to={`/catalog/Handtools`} className="btn-catalog" type='submit' >Handtools </Link>
                 <Link to={`/catalog/Оther`} className="btn-catalog" type='submit' >Оther</Link>
 
-
                 <ul >
-                 
-                    {products.length  > 0
+                    {products.length > 0
                         ? products.map(product =>
                             <li key={product._id}>
                                 <Publication {...product} />
                             </li>)
-                        : 
-                             <article className="no-available-publications">
-                                <h1>No product created yet.</h1>
-                                <a href="/create" className="create-pub">Create product</a>
-                            </article>
-                         
+                        :
+                        <article className="no-available-publications">
+                            <h1>No product created yet.</h1>
+                            <a href="/create" className="create-pub">Create product</a>
+                        </article>
                     }
                 </ul>
-
-
-
             </article >
         </section >
     )
