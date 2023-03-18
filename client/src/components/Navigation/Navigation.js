f939c47b546d5a525093dea1e4ed8f911e80bdd5
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-19 00:32:05 +0200

    update navbar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -12,13 +12,15 @@ export default function Navigation() {
                 {/* <!-- Do not forget to change the path to the image --> */}
                 <img src="/img/2.jpg" alt="logo" />
                 <a href="/">Wood World Bulgaria</a>
-                <span>Welcome, {"email"}</span>
+                <NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Welcome, {"email"}</NavLink>
                 <ul className="menu">
+
+
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/catalog">Gallery</NavLink></li>
 
                     {/* <!-- Logged users -->
      {{#if isAuthenticated}} */}
-                    
+
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/create">Create Product</NavLink></li>
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Profile</NavLink></li>
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/logout">Logout</NavLink></li>
@@ -26,7 +28,7 @@ export default function Navigation() {
      {{else}} */}
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/login">Login</NavLink></li>
                     <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/register">Register</NavLink></li>
-                    
+
                 </ul>
 
             </nav>
