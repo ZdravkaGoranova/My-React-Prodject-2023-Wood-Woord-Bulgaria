ed872545ab9e883a6bb2706489a9b34fbddef86f
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-20 21:16:16 +0200

    update

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,38 +1,36 @@
 import '../Home/home.css'
+import { AuthContext } from '../../contexts/AuthContext.js'
 
-
-
-import { Link, NavLink } from 'react-router-dom';
-
+import { useContext } from 'react'
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation() {
+    const { isAuthenticated, userEmail } = useContext(AuthContext)
+    // console.log(isAuthenticated);
+    // console.log(userEmail);
     return (
         <>
             <nav>
-                {/* <!-- Do not forget to change the path to the image --> */}
                 <img src="/img/2.jpg" alt="logo" />
                 <a href="/">Wood World Bulgaria</a>
-                <NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Welcome, {"email"}</NavLink>
                 <ul className="menu">
-
-
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/catalog">Gallery</NavLink></li>
-
-                    {/* <!-- Logged users -->
-     {{#if isAuthenticated}} */}
-
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/create">Create Product</NavLink></li>
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Profile</NavLink></li>
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/logout">Logout</NavLink></li>
-                    {/* <!-- Guest users -->
-     {{else}} */}
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/login">Login</NavLink></li>
-                    <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/register">Register</NavLink></li>
-
+                    {isAuthenticated ?
+                        <>
+                            <NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Welcome, {userEmail}!</NavLink>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/catalog">Gallery</NavLink></li>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/create">Create Product</NavLink></li>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/profile">Profile</NavLink></li>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/logout">Logout</NavLink></li>
+                        </>
+                        :
+                        <>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/catalog">Gallery</NavLink></li>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/login">Login</NavLink></li>
+                            <li><NavLink style={({ isActive }) => ({ color: isActive ? 'brown' : 'black' })} to="/register">Register</NavLink></li>
+                        </>
+                    }
                 </ul>
-
             </nav>
         </>
-
     )
 }
