02f8bc5cab3afe799f7b95c63938a2561bb5e62d
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-03-20 21:17:19 +0200

    update

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,5 +1,4 @@
 
-
 import '../Profile/profile.css'
 
 import Publication from '../Catalog/Publication/Publication.js'
@@ -7,8 +6,11 @@ import React, { useEffect, useState, useContext } from "react"
 import { Link } from 'react-router-dom';
 import { WoodContext } from '../../contexts/WoodContext.js'
 import MyProducts from './MyProducts.js';
-export default function Profile() {
+import { AuthContext } from '../../contexts/AuthContext.js'
 
+
+export default function Profile() {
+    const {  userEmail } = useContext(AuthContext)
     const { products } = useContext(WoodContext)
     console.log(products)
 
@@ -16,16 +18,16 @@ export default function Profile() {
         <section id="profile-page">
             <h1>Full Profile Information</h1>
             <section id="profilePage">
-                <div class="userInfo">
-                    <div class="avatar">
+                <div className="userInfo">
+                    <div className="avatar">
                         <img src="/img/profile.jpg" />
                     </div>
-                    <h2>Email: {"email"}</h2>
+                    <h2>Email: {userEmail}</h2>
                 </div>
-                <div class="board">
+                <div className="board">
                     {/* <!--If there are event--> */}
-                    {products.length == 0 && <div class="no-events">
-                        <p>This user has no products yet!</p>
+                    {products.length == 0 && <div className="no-events">
+                        <p>This user has no events yet!</p>
                     </div>}
 
                     {products.length > 0 &&
