396b368da00ca531d171670ab9b9f6e1266d63c4
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-04-07 14:22:48 +0300

    Create Home.test.js

---
+++ client/src/components/Home/Home.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import Home from './Home.js';
+import { PersonsContext } from '../../contexts/pesronsContext';
+
+describe('Home component', () => {
+  test('renders the Search component', () => {
+    // const { getByPlaceholderText } = render(
+    //   <PersonsContext.Provider value={{}}>
+    //     <Header />
+    //   </PersonsContext.Provider>
+    // );
+
+    // expect(getByPlaceholderText('Search')).toBeInTheDocument();
+  });
+});
